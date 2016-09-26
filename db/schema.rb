fb4ed624b6fc68ddd9fa671f06e01d946ae52ac4
jordanallain <jallain@gmail.com>
2016-09-26 16:36:49 -0400

    Add bio column to users table
    Include a column for users table to hold eventual bio users can
    fill in and edit.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160926170519) do
+ActiveRecord::Schema.define(version: 20160926203502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20160926170519) do
     t.string   "password_digest", null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "bio"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
