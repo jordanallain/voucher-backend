3efeee80e2fa52d0eb80cafcdff495ddb22db493
jordanallain <jallain@gmail.com>
2016-09-28 00:06:08 -0400

    Add user and instrument back to serializer
    define custom methods to grab correct user and instrument for
    join table serializer.

--- app/serializers/user_instrument_serializer.rb
+++ app/serializers/user_instrument_serializer.rb
@@ -3,11 +3,11 @@ class UserInstrumentSerializer < ActiveModel::Serializer
   has_one :user
   has_one :instrument
 
-  # def user
-  #   object.user.id
-  # end
-  #
-  # def instrument
-  #   object.instrument.id
-  # end
+  def user
+    object.user.id
+  end
+
+  def instrument
+    object.instrument.id
+  end
 end
